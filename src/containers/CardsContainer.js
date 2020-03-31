d7fb438d44c9a739c191fbe43831390869815ee5
Varun Swarup <varunswarup0@gmail.com>
2020-03-31 10:11:42 +0530

    Reselect Perfomance

--- src/containers/CardsContainer.js
+++ src/containers/CardsContainer.js
@@ -1,14 +1,13 @@
 import { connect } from 'react-redux';
 import Card from '../components/Card';
 import { removeCard } from '../actions/card-actions';
+import memoize from 'lodash/memoize';
+import { createSelector } from 'reselect';
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    card: state.cards.entities[ownProps.cardId],
-  };
+	return {
+		card: state.cards.entities[ownProps.cardId]
+	};
 };
 
-export default connect(
-  mapStateToProps,
-  { removeCard },
-)(Card);
+export default connect(mapStateToProps, { removeCard })(Card);
