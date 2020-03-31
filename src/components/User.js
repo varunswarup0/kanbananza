0a119ec417d59f76885c4abb72be0d07e43404e4
Varun Swarup <varunswarup0@gmail.com>
2020-03-31 09:45:03 +0530

    Setting Avatar

--- src/components/User.js
+++ src/components/User.js
@@ -2,30 +2,30 @@ import React, { Component } from 'react';
 import md5 from 'md5';
 
 const createProfileImageUrl = ({ email }) => {
-  const hash = md5(email.trim());
-  return `https://www.gravatar.com/avatar/${hash}`;
+	const hash = md5(email.trim());
+	return `https://www.gravatar.com/avatar/${hash}`;
 };
 
 class User extends Component {
-  handleChange = (event) => {
-    const { user, onUpdateUser } = this.props;
-    if (onUpdateUser) onUpdateUser({ ...user, name: event.target.value });
-  };
+	handleChange = event => {
+		const { user, onUpdateUser } = this.props;
+		if (onUpdateUser) onUpdateUser({ ...user, name: event.target.value });
+	};
 
-  render() {
-    const { user = {} } = this.props;
-    const profileImage = createProfileImageUrl(user);
+	render() {
+		const { user = {} } = this.props;
+		const profileImage = createProfileImageUrl(user);
 
-    return (
-      <article className="User">
-        <img className="User-picture" src={profileImage} alt={user.name} />
-        <div className="User-info">
-          <h4>{user.name || '[Placeholder]'}</h4>
-          <input type="text" value={user.name} onChange={this.handleChange} />
-        </div>
-      </article>
-    );
-  }
+		return (
+			<article className='User'>
+				<img className='User-picture' src={profileImage} alt={user.name} />
+				<div className='User-info'>
+					<h4>{user.name || '[Placeholder]'}</h4>
+					<input type='text' value={user.name} onChange={this.handleChange} />
+				</div>
+			</article>
+		);
+	}
 }
 
 export default User;
